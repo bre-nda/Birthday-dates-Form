1016686f83a6abf749140de6b362dd646fdfe791
bre-nda <brenda.kanale@student.moringaschool.com>
2021-07-18 22:23:03 +0300

    conocle the entered constants

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 var day = (((20 / 4) - 2 * 20 - 1) + ((21 / 4) + ((26 * (07 + 1) / 10)) + 17) % 7);
 console.log(day)
+    // recieve inputs
+function receiveInputs() {
+    const date = document.getElementById("start").value;
+    const female = document.getElementById("Female").value;
+    const male = document.getElementById("Male").value;
+    console.log(date + female + male)
+
+}
